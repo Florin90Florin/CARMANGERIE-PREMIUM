0b4fdfb9f3080e88a01a84a9e9c0395b799afe0f
Florin90Florin <venom_heart666@yahoo.com>
2026-07-25 16:34:10 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+simport { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import {
   getFirestore,
   collection,
